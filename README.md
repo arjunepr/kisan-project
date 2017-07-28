7882df6cc0b72fe8b731e77de9a00c7611b295b0
Arjun Raju Pillai <revarjunr@gmail.com>
2017-07-28 21:35:16 +0530

    Initial commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+Note that I've made use of a modified version of my weblack [template](https://github.com/arjunepr/webpack-wannabe-template).
+
+A copy of this project can be found in [this](https://github.com/arjunepr/kisan-project) repo.
+
---
+++ webpack.config.js
@@ -0,0 +1,243 @@
+const process = require('process');
+
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+const stylusAutoprefixer = require('autoprefixer-stylus');
+
+const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const webpack = require('webpack');
+
+const Merge = require('webpack-merge');
+
+const { join } = require('path');
+
+
+const commonConfig = {
+  entry: {
+    index: ['./src/app.js'],
+  },
+  output: {
+    path: join(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+    chunkFilename: '[name].bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.json', '.vue', '.html', '.styl'],
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js',
+      // inherits$: join(__dirname, 'node_modules/inherits')
+    }
+  },
+  node: {
+    fs: 'empty',
+    // child_process: 'empty',
+  },
+  module: {
+    rules: [
+
+      {
+        test: /\.js$/,
+        exclude: [
+          join(__dirname, 'node_modules'),
+        ],
+        use: ['babel-loader']
+      },
+
+      
+
+      // {
+      //   test: /\.html$/,
+      //   use: [
+      //     {
+      //       loader: "file-loader",
+      //       options: {
+      //         name: "[name].html",
+      //       },
+      //     },
+
+      //     {
+      //       loader: "extract-loader",
+      //     },
+
+      //     {
+      //       loader: "html-loader",
+      //       options: {
+      //         // attrs: ["img:src", "link:href"],
+      //         // interpolate: true,
+      //       },
+      //     },
+      //   ],
+      // },
+
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'images/[name].[ext]',
+            },
+          },
+          {
+            loader: 'image-webpack-loader',
+            query: {
+              progressive: true,
+              optimizationLevel: 12,
+              interlaced: false,
+              pngquant: {
+                quality: '65-70',
+                speed: 4
+              }
+            }
+          }
+        ]
+      },
+
+      {
+        test: /\.(ttf|eot|woff|woff2|otf)$/,
+        loader: 'file-loader',
+        options: {
+          name: 'fonts/[name].[ext]',
+        },
+      },
+
+
+    ]
+  },
+  plugins: [
+
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    }),
+
+    new ExtractTextPlugin('style.css'),
+
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    }),
+
+    new FriendlyErrorsPlugin()
+  ]
+};
+
+const devConfig = {
+  devtool: 'eval-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.styl$/,
+        use: ['style-loader', 'css-loader', 'stylus-loader'],
+      },
+
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ]
+  },
+
+  devServer: {
+    contentBase: join(__dirname, "dist"),
+    // publicPath: '/',
+    compress: true,
+    port: 8080,
+    historyApiFallback: true
+  },
+
+  entry: {
+    server: "webpack-dev-server/client?http://localhost:8080/"
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('development')
+      }
+    }),
+  ]
+};
+
+const prodConfig = {
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.styl$/,
+        use: ExtractTextPlugin.extract({
+          fallback: { loader: 'style-loader' },
+
+          use: ['css-loader', 'stylus-loader']
+        })
+      },
+
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: { loader: 'style-loader' },
+
+          use: ['css-loader']
+        })
+      },
+
+      {
+        test: /\.((html)|(js))$/,
+        enforce: 'pre',
+        exclude: /(node_modules|bower_components|\.spec\.js)/,
+        use: [
+          {
+            loader: 'webpack-strip-block'
+          }
+        ]
+      },
+
+
+    ],
+  },
+
+  plugins: [
+    //   new UglifyJSPlugin({
+    //   extractComments: true,
+    //   sourceMap: true
+    // })
+
+    new webpack.LoaderOptionsPlugin({
+      test: /\.styl$/,
+      stylus: {
+        // You can have multiple stylus configs with other names and use them
+        // with `stylus-loader?config=otherConfig`.
+        default: {
+          use: [stylusAutoprefixer({ browsers: ['last 3 versions'] })],
+        },
+      },
+    }),
+
+    new ExtractTextPlugin('style.css'),
+
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      beautify: false,
+      comments: false,
+      compress: {
+        warnings: false,
+        drop_console: true,
+        screw_ie8: true
+      },
+    }),
+
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+
+    new webpack.optimize.AggressiveMergingPlugin()
+  ]
+};
+
+
+module.exports = (process.env.NODE_ENV === 'development' ? Merge(commonConfig, devConfig) : Merge(commonConfig, prodConfig)); 
