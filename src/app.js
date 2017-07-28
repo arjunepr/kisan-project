ac636af7ac5432fd2af075f9618a25821fffbb60
Arjun Raju Pillai <revarjunr@gmail.com>
2017-07-29 00:13:42 +0530

    Remove foreign fonts. Make Helvetica finally work on header.

--- src/app.js
+++ src/app.js
@@ -8,12 +8,3 @@ import './styl/style';
 
 
 
-
-
-// Importing fonts
-import './fonts/AnonymousPro/Anonymous_Pro.ttf'
-
-// Just a way of avoiding boilerplate. Albeit a bit ugly..
-['B', 'BI', 'I'].forEach((piece) => {
-  import(`./fonts/AnonymousPro/Anonymous_Pro_${piece}.ttf`);
-});
