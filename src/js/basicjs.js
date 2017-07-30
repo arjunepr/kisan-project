dce04a513feba1f639d7389f366124b85b19056f
Arjun Raju Pillai <revarjunr@gmail.com>
2017-07-30 16:50:14 +0530

    Clean up files and almost finish project.

--- src/js/basicjs.js
+++ src/js/basicjs.js
@@ -1,6 +1,19 @@
+// This is to update the counter for the message textarea.
+
+const message = document.getElementById('message');
+const current = document.getElementById('current');
+
+message.addEventListener('input', (evt) => {
+  current.innerHTML = evt.target.value.length;
+})
+
+
+
+// This is for the menu to appear on mobiles and tablets on clicking the hamburger button.
+
 const ham = document.getElementById('hamburger');
 const navUl = document.getElementById('nav-ul');
 
-ham.addEventListener('click', (evt) => { 
+ham.addEventListener('click', (evt) => {
   navUl.classList.toggle('reveal');
 });
